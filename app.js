d46cdb8fd2aa12eae825be10e26c199aba1e9304
naivelove0822 <naivelove0822@gmail.com>
2022-03-05 16:38:07 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express()
 const mongoose = require('mongoose')
 const port = 3000
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo') 
+const bodyParser = require('body-parser')
 
-
+const Todo = require('./models/todo')
 
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
@@ -24,12 +24,27 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  // 另一種寫法
+  // const todo = new Todo({ name })
+  // return todo.save()
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 // 設定 port 3000
