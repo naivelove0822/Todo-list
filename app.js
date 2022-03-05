e997f89f3854eebd4c5b28d973ce483acb1330b2
naivelove0822 <naivelove0822@gmail.com>
2022-03-05 18:06:01 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -50,9 +50,9 @@ app.post('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
-  .lean()
-  .then((todo) => res.render('detail', { todo: todo}))
-  .catch(error => console.error(error)) // 錯誤處理
+    .lean()
+    .then((todo) => res.render('detail', { todo: todo }))
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 app.get('/todos/:id/edit', (req, res) => {
@@ -72,6 +72,14 @@ app.post('/todos/:id/edit', (req, res) => {
       return todo.save()
     })
     .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
