7e449b0d60b849083aad68d6a527f5bd4d70b27b
naivelove0822 <naivelove0822@gmail.com>
2022-03-09 19:13:50 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
 const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
 const routes = require('./routes')
+require('./config/mongoose')
 
-const Todo = require('./models/todo')
-
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
