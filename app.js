13dc52750dc8e9f0f9b6ecde9ad56e3996063400
naivelove0822 <naivelove0822@gmail.com>
2022-03-09 15:20:47 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'asc' }) //使物件照ID正序排列 (反序用desc)
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
@@ -66,7 +67,7 @@ app.get('/todos/:id/edit', (req, res) => {
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
-  
+
   return Todo.findById(id)
     .then(todo => {
       todo.name = name
